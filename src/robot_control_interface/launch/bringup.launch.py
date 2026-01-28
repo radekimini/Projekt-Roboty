7bfabd51b75d27fb4e84d257f886755b5231e4ad
Azathotr <Madgorbir@gmail.com>
2026-01-28 23:59:27 +0100

    Create bringup.launch.py
    to fix

---
+++ src/robot_control_interface/launch/bringup.launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    set_tb3_model = SetEnvironmentVariable(
+        name='TURTLEBOT3_MODEL',
+        value='burger'
+    )
+
+    set_gazebo_model_path = SetEnvironmentVariable(
+        name='GAZEBO_MODEL_PATH',
+        value='/opt/ros/humble/share/turtlebot3_gazebo/models'
+    )
+
+    set_gazebo_plugin_path = SetEnvironmentVariable(
+        name='GAZEBO_PLUGIN_PATH',
+        value='/opt/ros/humble/lib'
+    )
+
+    gazebo = ExecuteProcess(
+        cmd=[
+            'ros2', 'launch',
+            'turtlebot3_gazebo',
+            'empty_world.launch.py'
+        ],
+        output='screen'
+    )
+
+    click_node = Node(
+        package='robot_control_interface',
+        executable='click_control_node',
+        output='screen'
+    )
+
+    return LaunchDescription([
+        set_tb3_model,
+        set_gazebo_model_path,
+        set_gazebo_plugin_path,
+        gazebo,
+        click_node,
+        odom_logger
+    ])
